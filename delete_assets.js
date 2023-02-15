81268c30cfcf0ee90d16ec34e052cb4ffd93c3e6
Siddharth Shukla <61881540+gunner26735@users.noreply.github.com>
2023-02-15 23:19:51 +0530

    Hided api key

--- delete_assets.js
+++ delete_assets.js
@@ -11,7 +11,7 @@ headers : {
     for(var i=6;i<data.data.length;i++){
         console.log(data.data[i].id);
         axios.delete(`https://livepeer.studio/api/asset/${data.data[i].id}`,{ headers : {
-            'Authorization' : 'Bearer 2df1219b-52f9-43bb-b5cb-bd8d7b9998a8'
+            'Authorization' : 'Bearer API KEY'
         }})
     }
 })
